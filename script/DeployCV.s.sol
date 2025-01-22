98f65c10536a5a1bdd1b403cdd02987414acf76c
junse0m <quasar125@naver.com>
2025-01-22 10:09:24 +0900

    lowercaseCV

--- script/DeployCV.s.sol
+++ script/DeployCV.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
-import {CV} from "../src/CV.sol";
+import {CV} from "../src/cv.sol";
 
 contract Deploy is Script {
     function run() public {
